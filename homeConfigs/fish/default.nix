bbbfe1878561d04f1347660769b26962314d5594
bonticelli <kandelakitina@gmail.com>
2024-03-05 19:32:00 +0000

    Add nix.fish fish plugin

--- homeConfigs/fish/default.nix
+++ homeConfigs/fish/default.nix
@@ -60,6 +60,15 @@
           sha256 = "sha256-Hq9UXB99kmbWKUVFDeJL790P8ek+xZR5LDvS+Qih+N4=";
         };
       }
+      {
+        name = "nix.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "kidonng";
+          repo = "nix.fish";
+          rev = "ad57d970841ae4a24521b5b1a68121cf385ba71e";
+          sha256 = "13x3bfif906nszf4mgsqxfshnjcn6qm4qw1gv7nw89wi4cdp9i8q";
+        };
+      }
     ];
   };
 }
