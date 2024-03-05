b14b9e23c5736d66de93a12c5ca005ef7a779ac2
bonticelli <kandelakitina@gmail.com>
2024-03-05 20:17:56 +0000

    Seem to have added zjstatus

--- homeConfigs/zellij/default.nix
+++ homeConfigs/zellij/default.nix
@@ -1,6 +1,36 @@
-{ config, lib, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
+  
+  home.packages = [
+    # pkgs.tmate
+    inputs.zjstatus.packages.${pkgs.system}.default
+  ];
+
+  xdg.configFile = {
+    "zellij/config.kdl".source = ./config.kdl;
+    "zellij/layouts/mine.kdl".text = ''
+      layout {
+       tab {
+      	 pane
+       }
+
+       default_tab_template {
+      	 pane size=1 borderless=true {
+      		 plugin location="zellij:compact-bar"
+      	 }
+      	 children
+       }
+      }
+    '';
+  };
+  
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
