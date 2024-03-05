10b6f6fe4a30d2efa21be3cfc78544bef47da71f
bonticelli <kandelakitina@gmail.com>
2024-03-05 20:18:44 +0000

    Add cachix servers

--- thinkpad/configuration.nix
+++ thinkpad/configuration.nix
@@ -92,6 +92,16 @@
     experimental-features = "nix-command flakes";
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
+
+    # adding cachix servers for quick binary download
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   # FIXME: Add the rest of your current configuration
