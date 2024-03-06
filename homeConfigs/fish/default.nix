b8ce960eb504927becefdba802a7ded4c279e35b
bonticelli <kandelakitina@gmail.com>
2024-03-06 15:11:29 +0000

    Change nix.fish sha256 (it still does not seem to work though)

--- homeConfigs/fish/default.nix
+++ homeConfigs/fish/default.nix
@@ -66,7 +66,7 @@
           owner = "kidonng";
           repo = "nix.fish";
           rev = "ad57d970841ae4a24521b5b1a68121cf385ba71e";
-          sha256 = "13x3bfif906nszf4mgsqxfshnjcn6qm4qw1gv7nw89wi4cdp9i8q";
+          sha256 = "sha256-GMV0GyORJ8Tt2S9wTCo2lkkLtetYv0rc19aA5KJbo48=";
         };
       }
     ];
