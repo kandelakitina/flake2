f77ced3ece50e2b03cd5f7257b59f4b28413dfbd
bonticelli <kandelakitina@gmail.com>
2024-03-07 08:28:04 +0000

    Restructured atuin to compile custom package inside atuin.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 {pkgs ? (import ../nixpkgs.nix) {}}: {
-  atuin-export-fish = pkgs.callPackage ./atuin-export-fish-history.nix {};
+  # atuin-export-fish = pkgs.callPackage ./atuin-export-fish-history.nix {};
   # headsetcontrol2 = pkgs.callPackage ./headsetcontrol.nix {};
   # all-ways-egpu = pkgs.callPackage ./all-ways-egpu.nix {};
   # copilotchat-nvim = pkgs.callPackage ./copilotchat-nvim.nix {};
