7543202377a17834e333097be2994631e1d043a7
bonticelli <kandelakitina@gmail.com>
2024-03-12 14:03:43 +0300

    Updated colornames in global theme

--- users/boticelli/colorschemes/iterm.nix
+++ users/boticelli/colorschemes/iterm.nix
@@ -1,21 +1,21 @@
 {
   slug = "iterm";
   palette = {
-    base00 = "#101421"; # background
-    base01 = "#2e2e2e"; # black
-    base02 = "#565656"; # dark gray - lighter black
-    base03 = "#7b5cb0"; # purple
-    base04 = "#fffbf6"; # foreground
-    base05 = "#ffffff"; # white - primary foreground
-    base06 = "#e5e9f0"; # light gray - secondary foreground
-    base07 = "#a47de9"; # pink
-    base08 = "#eb4129"; # red
-    base09 = "#f6c744"; # orange
-    base0A = "#f9da6a"; # yellow - alternative highlight
-    base0B = "#abe047"; # green
-    base0C = "#64dbed"; # cyan
-    base0D = "#47a0f3"; # blue
-    base0E = "#ec5357"; # light red - error/warning
-    base0F = "#49a4f8"; # bright blue - information/interaction
+    base00 = "#101421"; # Deep Blue (background)
+    base01 = "#2e2e2e"; # Dark Gray (acts as black)
+    base02 = "#565656"; # Medium Gray
+    base03 = "#7b5cb0"; # Purple
+    base04 = "#fffbf6"; # Off White 
+    base05 = "#ffffff"; # White
+    base06 = "#e5e9f0"; # Light Gray
+    base07 = "#a47de9"; # Lavender
+    base08 = "#eb4129"; # Red
+    base09 = "#f6c744"; # Orange
+    base0A = "#f9da6a"; # Yellow
+    base0B = "#abe047"; # Green
+    base0C = "#64dbed"; # Cyan
+    base0D = "#47a0f3"; # Blue
+    base0E = "#ec5357"; # Bright Red
+    base0F = "#49a4f8"; # Bright Blue
   };
 }
