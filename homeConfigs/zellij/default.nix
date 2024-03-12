b4ef3c59aca2d891d826c9e7e7bd2ca82fd9acd6
bonticelli <kandelakitina@gmail.com>
2024-03-12 12:34:45 +0300

    Add custom keybinds to zellij

--- homeConfigs/zellij/default.nix
+++ homeConfigs/zellij/default.nix
@@ -16,23 +16,23 @@ in {
     inputs.zjstatus.packages.${pkgs.system}.default
   ];
 
-  # xdg.configFile = {
-  #   "zellij/config.kdl".source = ./config.kdl;
-  #   "zellij/layouts/mine.kdl".text = ''
-  #     layout {
-  #      tab {
-  #     	 pane
-  #      }
+  xdg.configFile = {
+    "zellij/config.kdl".text = builtins.readFile ./config.kdl;
+    "zellij/layouts/mine.kdl".text = ''
+      layout {
+       tab {
+      	 pane
+       }
 
-  #      default_tab_template {
-  #     	 pane size=1 borderless=true {
-  #     		 plugin location="zellij:compact-bar"
-  #     	 }
-  #     	 children
-  #      }
-  #     }
-  #   '';
-  # };
+       default_tab_template {
+      	 pane size=1 borderless=true {
+      		 plugin location="zellij:compact-bar"
+      	 }
+      	 children
+       }
+      }
+    '';
+  };
 
   programs.zellij = {
     enable = true;
@@ -43,31 +43,6 @@ in {
       themes = import ./themes.nix {inherit colorScheme;};
       defaultShell = "fish";
       copyOnSelect = true;
-
-      # keybinds = {
-      #   unbind = ["Ctrl q"];
-      #   normal = {};
-      #   locked = {
-      #     clearDefaults = true;
-      #     bind = [
-      #       {
-      #         key = "Alt u";
-      #         action = "SwitchToMode 'normal'";
-      #       }
-      #     ];
-      #   };
-      #   resize = {
-      #     clearDefaults = true;
-      #     bind = [
-      #       {
-      #         key = "Esc";
-      #         action = {SwitchToMode = "Normal";};
-      #       }
-      #       # Additional bindings...
-      #     ];
-      #   };
-      #   # Additional modes and bindings...
-      # };
     };
   };
 }
