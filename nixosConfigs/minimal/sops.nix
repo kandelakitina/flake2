dc0615f013361b17d48a7588ba358c952cb9d5dd
boticelli <kandelakitina@gmail.com>
2024-03-15 11:14:22 +0300

    Change sops.nix to refer to openssh.nix to collect ssh keys for age

--- nixosConfigs/minimal/sops.nix
+++ nixosConfigs/minimal/sops.nix
@@ -1,13 +1,29 @@
-{inputs, ...}: {
+# {inputs, ...}: {
+#   imports = [
+#     inputs.sops-nix.nixosModules.sops
+#   ];
+
+#   sops = {
+#     gnupg = {
+#       home = "~/.gnupg";
+#       sshKeyPaths = [];
+#     };
+#     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+#   };
+# }
+
+{ inputs, lib, config, ... }:
+let
+  isEd25519 = k: k.type == "ed25519";
+  getKeyPath = k: k.path;
+  keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
+in
+{
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
 
   sops = {
-    gnupg = {
-      home = "~/.gnupg";
-      sshKeyPaths = [];
-    };
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    age.sshKeyPaths = map getKeyPath keys;
   };
 }
