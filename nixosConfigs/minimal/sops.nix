f40e6920964cc8bd2d683704ab2475e19d9c08d4
boticelli <kandelakitina@gmail.com>
2024-03-18 14:49:34 +0300

    Update sops

--- nixosConfigs/minimal/sops.nix
+++ nixosConfigs/minimal/sops.nix
@@ -4,11 +4,11 @@
   ];
 
   sops = {
-    # gnupg = {
-    #   home = "~/.gnupg";
-    #   sshKeyPaths = [];
-    # };
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    gnupg = {
+      home = "~/.gnupg";
+      sshKeyPaths = [];
+    };
+    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519"];
   };
 }
 
