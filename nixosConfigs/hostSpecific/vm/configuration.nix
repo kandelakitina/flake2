f8625ae411a5330a15145a7b7b1349a48d94f7c2
Eremenko Mikhail Yu. <MiEremenko@msk.lo>
2024-03-29 20:55:36 +0300

    Restarting building VM config

--- nixosConfigs/hostSpecific/vm/configuration.nix
+++ nixosConfigs/hostSpecific/vm/configuration.nix
@@ -1,20 +1,24 @@
-# configuration.nix
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
-  config,
+  outputs,
   lib,
+  config,
   pkgs,
+  # hostName,
   ...
 }: {
+  # You can import other NixOS modules here
   imports = [
     # inputs.hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
     inputs.disko.nixosModules.disko
-    (import ../../../diskoConfigs/btfrs.nix {device = "/dev/vda";})
-
+    
     ./hardware-configuration.nix
 
     ../../userSpecific/boticelli.nix
 
+    (import ../../../diskoConfigs/btfrs.nix { device = "/dev/vda"; })
 
     ../../minimal/default.nix
 
@@ -31,85 +35,26 @@
     # ../../optional/virtualisation.nix
   ];
 
-  # customize kernel version
-  # boot.kernelPackages = pkgs.linuxPackages_5_15;
-
   networking = {
     hostName = "vm";
     # inherit hostName;
   };
 
-  # fileSystems."/" = {
-  #   device = "/dev/root_vg/root";
-  #   fsType = "btrfs";
-  #   options = [ "subvol=root" ];
-  # };
-
-  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
-
-  # fileSystems."/persistent" = {
-  #   device = "/dev/root_vg/root";
-  #   neededForBoot = true;
-  #   fsType = "btrfs";
-  #   options = [ "subvol=persistent" ];
-  # };
-
-  # fileSystems."/nix" = {
-  #   device = "/dev/root_vg/root";
-  #   fsType = "btrfs";
-  #   options = [ "subvol=nix" ];
-  # };
-
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-label/boot";
-  #   fsType = "vfat";
-  # };
-
-  # boot = {
-  #   kernelParams = [
-  #     "amdgpu.sg_display=0"
-  #     "resume_offset=533760"
-  #   ];
-  #   blacklistedKernelModules = ["hid-sensor-hub"];
-  #   supportedFilesystems = lib.mkForce ["btrfs"];
-  #   kernelPackages = pkgs.linuxPackages_latest;
-  #   loader = {
-  #     systemd-boot.enable = true;
-  #     efi.canTouchEfiVariables = true;
-  #   };
-  #   # resumeDevice = "/dev/disk/by-label/nixos";
-  # };
-
-  users.users = {
-    admin = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-      password = "admin";
-      group = "admin";
+  boot = {
+    kernelParams = [
+      "amdgpu.sg_display=0"
+      "resume_offset=533760"
+    ];
+    blacklistedKernelModules = ["hid-sensor-hub"];
+    supportedFilesystems = lib.mkForce ["btrfs"];
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
+    # resumeDevice = "/dev/disk/by-label/nixos";
   };
 
-  users.groups.admin = {};
-
-  virtualisation.vmVariant = {
-    # following configuration is added only when building VM with build-vm
-    virtualisation = {
-      memorySize = 4048; # Use 2048MiB memory.
-      cores = 4;
-      graphics = false;
-    };
-  };
-
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = true;
-  };
-
-  networking.firewall.allowedTCPPorts = [22];
-  environment.systemPackages = with pkgs; [
-    htop
-    helix
-  ];
-
-  system.stateVersion = "23.05";
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
 }
