ab50f2fba390f9623a4bf7c434f647fbb4a347a7
Eremenko Mikhail Yu. <MiEremenko@msk.lo>
2024-03-30 10:58:06 +0300

    Add fileSystems to vm/configuration.nix

--- nixosConfigs/hostSpecific/vm/configuration.nix
+++ nixosConfigs/hostSpecific/vm/configuration.nix
@@ -67,6 +67,18 @@
     };
   };
 
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  
+  swapDevices = [ ];
+
   # boot = {
   #   kernelParams = [
   #     "amdgpu.sg_display=0"
